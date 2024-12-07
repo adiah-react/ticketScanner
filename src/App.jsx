4f76809862ec7ff0bf6e7354a135d2e396cbfc95
nathanieladiah <nathanieladiah@gmail.com>
2024-12-07 04:01:18 -0400

    Add basic route scaffolding

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/scan" element={<h1>Scanner</h1>} />
+        </Routes>
+        <div>Navbar</div>
+      </Router>
+      <ToastContainer />
+    </>
+  );
 };
 export default App;
